f5eba7be1cb9608c9c53b6ab972cc93508d23769
NidhiThakkar <thakkarnidhi20@outlook.com>
2016-08-17 13:08:23 -0400

    Initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,33 @@
+
+/**
+ * FileName: app.js
+ *
+ * @author Nidhi Thakkar
+ * @date August 17, 2016
+ *
+ * StudentID: 300872151
+ *
+ * @description This file is the main javascript file for the web site
+ */
+var core;
+(function (core) {
+    "use strict";
+    var canvas;
+    var stage;
+    // app entry function
+    function init() {
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", gameLoop);
+        main();
+    }
+    function gameLoop() {
+        stage.update();
+    }
+    function main() {
+    }
+    window.addEventListener("load", init);
+})(core || (core = {}));
+//# sourceMappingURL=app.js.map
---
+++ Scripts/app.ts
@@ -0,0 +1,39 @@
+/**
+ * FileName: app.ts
+ * 
+ * @author Nidhi Thakkar
+ * @date August 17, 2016
+ * 
+ * StudentID: 300872151
+ * 
+ * @description This file is the main javascript file for the web site
+ */
+module core {
+    "use strict";
+
+    let canvas: HTMLElement;
+    let stage: createjs.Stage;
+
+
+    // app entry function
+    function init(): void {
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20); 
+        createjs.Ticker.framerate = 60; 
+        createjs.Ticker.on("tick", gameLoop);
+        main();
+    }
+ 
+    function gameLoop(): void {
+
+        stage.update(); 
+    }
+
+    function main(): void {
+        
+    }
+
+    window.addEventListener("load", init);
+
+}
